53818c06212b0faca8e7af7de31ffc260d3e33b5
matheusdsoares <matheusd.oliveira@outlook.com>
2026-02-07 00:34:37 -0300

    Resolvendo conflitos no Program.cs após mover para dia06

---
+++ dia06/MinhaApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using MinhaApi.Data; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+// Registro do DbContext com Npgsql
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var cs = "Host=localhost;Port=5432;Database=minhaapi_db;Username=postgres;Password=postgres"; 
+    options
+        .UseNpgsql(cs)
+        .UseSnakeCaseNamingConvention();
+});
+
+
+// Recomendação do Npgsql para compatibilidade de timestamp (se aplicável)
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+// Mapear controllers
+app.MapControllers();
+
+app.Run();
